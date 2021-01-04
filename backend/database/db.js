bcbd61fad95dddcc668772ca2232a270516070de
Vivi <vivianne.traore@hotmail.fr>
2021-01-04 11:31:28 +0100

    mddleware auth

--- backend/database/db.js
+++ backend/database/db.js
@@ -10,6 +10,14 @@ const connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     console.log("Connected to database");
+
+    connection.query(`CREATE TABLE IF NOT EXISTS adminn
+    (id_admin INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+    admin_lastname VARCHAR(150) NOT NULL, 
+    admin_firstname VARCHAR(150) NOT NULL, 
+    admin_email VARCHAR(250) NOT NULL, 
+    admin_password VARCHAR(250) NOT NULL)`); 
+
 });
 
 module.exports = connection;
--- backend/middleware/authtoken.js
+++ backend/middleware/authtoken.js
@@ -1,6 +1,22 @@
 const config = require('../modules/config');
 const jwt = require('jsonwebtoken');
 
+const authJWT = (req, res, next) => {
+    const decocedToken = req.headers.authorization;
+
+    if (decocedToken) {
+        const token = decocedToken.split(' ')[1];
+
+        jwt.verify(token, config.secret, (err, user) => {
+            if (err) {
+                return res.send.status(403).json({error: 'Unauthorized'});
+            }
+            next();
+        });
+    } else {
+        res.send.status(401);
+    }
+};
 
 module.exports 
 
